040181403e2f0513e34099c61e56cbdfbcb818e5
alexvedia <alexvediacastro@gmail.com>
2024-06-14 21:21:22 -0400

    Ahora iniciamos con la creacion de el modelo de ordenes

--- components/order/ProductDetails.tsx
+++ components/order/ProductDetails.tsx
@@ -1,14 +1,25 @@
 import { XCircleIcon, PlusIcon, MinusIcon } from "@heroicons/react/24/outline"
 import { OrderItem } from "@/src/types"
 import { formatCurrency } from "@/src/utils"
-
+import { useStore } from "@/src/store"
+import { useMemo } from "react"
 
 type ProductDetailsProps = {
     item: OrderItem
 }
 
+const MIN_ITEMS = 1
+const MAX_ITEMS = 5
+
 export default function ProductDetails({ item }: ProductDetailsProps) {
-    console.log(item)
+    const increaseQuantity = useStore((state) => state.increaseQuantity)
+    const decreaseQuantity = useStore((state) => state.decreaseQuantity)
+    const removeItem = useStore((state) => state.removeItem)
+    const disableDecreaseButton = useMemo(() => item.quantity === MIN_ITEMS, [item])
+    const disableIncreaseButton = useMemo(() => item.quantity === MAX_ITEMS, [item])
+
+
+
     return (
         <div className="shadow space-y-1 p-4 bg-white  border-t border-gray-200 ">
             <div className="space-y-4">
@@ -17,7 +28,7 @@ export default function ProductDetails({ item }: ProductDetailsProps) {
 
                     <button
                         type="button"
-                        onClick={() => { }}
+                        onClick={() => removeItem(item.id)}
                     >
                         <XCircleIcon className="text-red-600 h-8 w-8" />
                     </button>
@@ -28,7 +39,9 @@ export default function ProductDetails({ item }: ProductDetailsProps) {
                 <div className="flex gap-5 px-10 py-2 bg-gray-100 w-fit rounded-lg">
                     <button
                         type="button"
-                        onClick={() => { }}
+                        onClick={() => decreaseQuantity(item.id)}
+                        disabled={disableDecreaseButton}
+                        className="disabled:opacity-20"
                     >
                         <MinusIcon className="h-6 w-6" />
                     </button>
@@ -39,7 +52,9 @@ export default function ProductDetails({ item }: ProductDetailsProps) {
 
                     <button
                         type="button"
-                        onClick={() => { }}
+                        onClick={() => increaseQuantity(item.id)}
+                        className="disabled:opacity-10"
+                        disabled={disableIncreaseButton}
                     >
                         <PlusIcon className="h-6 w-6" />
                     </button>
