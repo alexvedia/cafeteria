4cb5201a231eb5d794ab059f3ce10f09d34f2a28
alexvedia <alexvediacastro@gmail.com>
2024-06-16 18:18:31 -0400

    toca hacer la validacion de datos con ZOD

---
+++ actions/create-order-action.ts
@@ -0,0 +1,6 @@
+"use server"
+
+
+export async function createOrder(){
+    console.log('desed create-order-action.ts')
+}
--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -3,33 +3,61 @@ import { useStore } from "@/src/store"
 import ProductDetails from "./ProductDetails"
 import { useMemo } from "react"
 import { formatCurrency } from "@/src/utils"
+import { createOrder } from "@/actions/create-order-action"
 
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order)
-  const total = useMemo(() => order.reduce((total, item ) => total + (item.quantity * item.price), 0), [order])
+  const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+
+  const handleCreateOrder = (formData: FormData) => {
+    console.log(formData.get('name'))
+
+    createOrder()
+  }
 
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
 
-      { order.length === 0 ? <p className="text-center my-10">El carrito esta vacio</p> : (
+      {order.length === 0 ? <p className="text-center my-10">El pedido esta vacio</p> : (
         <div className="mt-5">
           {order.map(item => (
-            <ProductDetails 
-            key={item.id}
-            item={item}
+            <ProductDetails
+              key={item.id}
+              item={item}
             />
           ))}
 
-<p className="text-2xl mt-20 text-center">
-  Total a pagar: {''}
-  <span className="font-bold">{formatCurrency(total)}</span>
-</p>
+          <p className="text-2xl mt-20 text-center">
+            Total a pagar: {''}
+            <span className="font-bold">{formatCurrency(total)}</span>
+          </p>
 
+          <form
+            className="w-full mt-10 space-y-5"
+            action={handleCreateOrder}
+          >
+
+<input 
+type="text" 
+placeholder="Tu nombre crack"
+className="bg-white border bordre-gray-100 p-2 w-full" 
+name="name"
+/>
+
+
+            <input
+              type="submit"
+              className="py-2 rounded uppercase text-white bg-black
+               w-full text-center cursor-pointer font-bold"
+              value="Confirmar Pedido"
+            />
+
+          </form>
 
         </div>
-      ) }
+      )}
 
     </aside>
   )
